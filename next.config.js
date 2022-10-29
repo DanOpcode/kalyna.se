f1b9f16b0ae929370a258b9c2ae766de9ffc3b22
Daniel Jonsson <daniel@jonsson.xyz>
2022-10-29 20:16:01 +0200

    Add locale

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const withExportImages = require('next-export-optimize-images')
 const nextConfig = withExportImages({
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ["sv"],
+    defaultLocale: "sv",
+  },
 })
 
 module.exports = nextConfig

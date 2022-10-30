f3c2c71836911c8436241d78a7aecdaa86d4d8b6
Daniel Jonsson <daniel@jonsson.xyz>
2022-10-30 17:47:09 +0100

    Move images

---
+++ docs/_next/static/0SCA_EQ6m--AFlh3-Xnis/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/957-5bc551c4e8130f34.js","static/chunks/pages/index-b6dfb634b778a136.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

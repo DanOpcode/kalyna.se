129e68fca166b34889ed650166890f64866ceb66
Daniel Jonsson <daniel@jonsson.xyz>
2022-11-02 22:57:48 +0100

    Export new version of the site

---
+++ docs/_next/static/u5kHoQv2ATiFCTI581vJw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/69-75679bb6ad1f3925.js","static/chunks/pages/index-72c4dd66853be2c2.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

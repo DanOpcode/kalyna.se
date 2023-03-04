3fe364ee6496656da0c8a0c8ad21c137b4040480
Daniel Jonsson <daniel@jonsson.xyz>
2023-03-04 14:15:55 +0100

    Fix margin in bottom to company logo

--- pages/index.js
+++ pages/index.js
@@ -253,7 +253,7 @@ export default function Home() {
 
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div className="col-md-4 d-flex align-items-center">
-          <Link href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+          <Link href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1" style={{marginTop: 4 }}>
             <Image className="mb-2" src={LogoImage} width="24" height="24" alt="Företagsloggo för Kalyna Solutions AB." />
           </Link>
           <span className="mb-3 mb-md-0 text-muted">&copy; 2022 Kalyna Solutions AB</span>
